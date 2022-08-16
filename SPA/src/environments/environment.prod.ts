9e88e54ea27d6c304d3dc78db113a42cef012cba
Clerson Clervius Verdieu Vincent <soncler05@gmail.com>
2022-08-15 22:00:31 -0500

    update spa workflow

--- SPA/src/environments/environment.prod.ts
+++ SPA/src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "", // Change this to the address of your backend API if different from frontend address
+  tokenUrl: "", // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
+  loginUrl: '/login',
+  openIdConnect: {
+    google: {
+      name:"google",
+      issuer:"https://accounts.google.com",
+      clientId: '795536398021-pr0jruj56qdk6phcm1ed9jjbabthv0jb.apps.googleusercontent.com'
+    },
+    facebook: {
+      name:"facebook",
+      clientId: "326368018362079"
+    }
+  }
 };
