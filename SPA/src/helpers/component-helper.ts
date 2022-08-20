ae461f38818d135a0cc9883607cc7400878ae288
Clerson Clervius Verdieu Vincent <soncler05@gmail.com>
2022-08-20 10:52:27 -0500

    upgrade to angular 10

--- SPA/src/helpers/component-helper.ts
+++ SPA/src/helpers/component-helper.ts
@@ -1,6 +1,8 @@
-import { OnDestroy } from "@angular/core";
+import { Injectable, OnDestroy } from "@angular/core";
 import { Subject } from "rxjs";
 
+// TODO: Add Angular decorator.
+@Injectable()
 export abstract class ComponentHelper implements OnDestroy {
     protected readonly unsubscribe$ = new Subject<void>();
     protected readonly hello = 'HelloOOO!!!';
